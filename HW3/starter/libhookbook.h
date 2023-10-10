f44b70df2e63d988c7d957a0b9313b9185e321a6
unknown <mabrell>
2023-10-09 23:24:22 -0400

    beginning of hw3

---
+++ HW3/starter/libhookbook.h
@@ -0,0 +1,11 @@
+#ifndef __LIBHOOKBOOK_H__
+#define __LIBHOOKBOOK_H__
+
+/*
+Use this file to encapsulate generally useful functionality that doesn't belong
+in either pirate.h, pirate_list.h, or skills_list.h.
+
+Author: [your name here]
+*/
+
+#endif
---
+++ HW3/starter/skills_list.h
@@ -0,0 +1,15 @@
+#ifndef __SKILLS_LIST_H__
+#define __SKILLS_LIST_H__
+
+/*
+Header file for skills list for Hookbook assignments for CPSC 223 Fall 2023.
+
+Author: [your name here]
+*/
+
+/**
+ * Type of a list of skills, as an opaque struct.
+ */
+typedef struct skills_list_instance_t skills_list;
+
+#endif
